6669553c7c664307864c5416d4e2047a6bb19207
praglody <praglody@163.com>
2019-05-23 09:03:43 +0800

    remove dns server

--- config/config.go
+++ config/config.go
@@ -8,15 +8,15 @@ import (
 const WorkDir = "/data/software"
 
 type SsConfig struct {
-	Server           string         `json:"server"`
-	PortPassword     map[int]string `json:"port_password"`
-	Method           string         `json:"method"`
-	Timeout          int            `json:"timeout"`
-	FastOpen         bool           `json:"fast_open"`
-	DnsServer        [2]string      `json:"dns_server"`
-	TunnelRemote     string         `json:"tunnel_remote"`
-	TunnelRemotePort int            `json:"tunnel_remote_port"`
-	TunnelPort       int            `json:"tunnel_port"`
+	Server       string         `json:"server"`
+	PortPassword map[int]string `json:"port_password"`
+	Method       string         `json:"method"`
+	Timeout      int            `json:"timeout"`
+	FastOpen     bool           `json:"fast_open"`
+	// DnsServer        [2]string      `json:"dns_server"`
+	TunnelRemote     string `json:"tunnel_remote"`
+	TunnelRemotePort int    `json:"tunnel_remote_port"`
+	TunnelPort       int    `json:"tunnel_port"`
 }
 
 type UpstreamServer struct {
@@ -27,16 +27,16 @@ type UpstreamServer struct {
 }
 
 type LocalConfig struct {
-	Upstream         []UpstreamServer `json:"upstream"`
-	LocalAddress     string           `json:"local_address"`
-	LocalPort        int              `json:"local_port"`
-	Method           string           `json:"method"`
-	Timeout          int              `json:"timeout"`
-	FastOpen         bool             `json:"fast_open"`
-	DnsServer        [2]string        `json:"dns_server"`
-	TunnelRemote     string           `json:"tunnel_remote"`
-	TunnelRemotePort int              `json:"tunnel_remote_port"`
-	TunnelPort       int              `json:"tunnel_port"`
+	Upstream     []UpstreamServer `json:"upstream"`
+	LocalAddress string           `json:"local_address"`
+	LocalPort    int              `json:"local_port"`
+	Method       string           `json:"method"`
+	Timeout      int              `json:"timeout"`
+	FastOpen     bool             `json:"fast_open"`
+	//DnsServer        [2]string        `json:"dns_server"`
+	TunnelRemote     string `json:"tunnel_remote"`
+	TunnelRemotePort int    `json:"tunnel_remote_port"`
+	TunnelPort       int    `json:"tunnel_port"`
 }
 
 func GetConfig() *SsConfig {
@@ -46,7 +46,7 @@ func GetConfig() *SsConfig {
 		"aes-256-cfb",
 		600,
 		false,
-		[2]string{"8.8.8.8", "223.5.5.5"},
+		//[2]string{"8.8.8.8", "223.5.5.5"},
 		"8.8.8.8",
 		53,
 		53,
@@ -61,7 +61,7 @@ func GetLocalConfig() *LocalConfig {
 		"aes-256-cfb",
 		600,
 		false,
-		[2]string{"8.8.8.8", "223.5.5.5"},
+		//[2]string{"8.8.8.8", "223.5.5.5"},
 		"8.8.8.8",
 		53,
 		53,
